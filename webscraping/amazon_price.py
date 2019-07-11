9b94e765634ded9d3de78b083aa58cbc4efaf673
mshahbaazshareef <mshahbaazshareef@gmail.com>
2019-07-11 22:29:36 +0530

    price and product scrape functionality working

---
+++ webscraping/amazon_price.py
@@ -0,0 +1,26 @@
+#testing some git yeah !!!
+
+import requests
+import smtplib
+
+from bs4 import BeautifulSoup
+
+url = 'https://www.amazon.in/Apple-iPhone-Silver-64GB-Storage/dp/B0711T2L8K/ref=sr_1_1_sspa?keywords=iphone+x&qid=1562856325&s=electronics&sr=1-1-spons&psc=1'
+
+headers = {"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
+
+page = requests.get(url, headers=headers )
+
+soup = BeautifulSoup(page.content ,'html5lib' )
+#BeautifulSoup(raw, )
+
+title = soup.find(id="productTitle").get_text()
+price =soup.find(id="priceblock_ourprice").get_text()
+
+convprice = int(price[2:4])
+
+print(title.strip())
+print(convprice)
+y = 60
+if (convprice> y):
+    print("price change")
